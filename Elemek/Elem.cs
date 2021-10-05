986ef12e6f83f247f79f737e1e4755a2047898e8
Neomonolith <90251446+Neomonolith@users.noreply.github.com>
2021-10-05 10:24:53 +0200

    Add project files.

---
+++ Elemek/Elem.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elemek
+{
+  class Elem
+  {
+    private string ev;
+
+    public string Ev
+    {
+      get { return ev; }
+      set { ev = value; }
+    }
+
+    private string nev;
+
+    public string Nev
+    {
+      get { return nev; }
+      set { nev = value; }
+    }
+
+    private string vegyjel;
+
+    public string Vegyjel
+    {
+      get { return vegyjel; }
+      set { vegyjel = value; }
+    }
+
+    private int rszam;
+
+    public int Rszam
+    {
+      get { return rszam; }
+      set { rszam = value; }
+    }
+
+
+    private string tudos;
+
+    public string Tudos
+    {
+      get { return tudos; }
+      set { tudos = value; }
+    }
+
+    public Elem(string ev, string nev, string vegyjel, int rszam, string tudos)
+    {
+      this.ev = ev;
+      this.nev = nev;
+      this.vegyjel = vegyjel;
+      this.rszam = rszam;
+      this.tudos = tudos;
+    }
+  }
+}
---
+++ Elemek/Kemia.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Elemek
+{
+  class Program
+  {
+
+
+    static List<Elem> Elemek = new List<Elem>();
+    static void Main(string[] args)
+    {
+     
+
+      masodikfeladat();
+      harmadikfeladat();
+
+      Console.ReadKey();
+    }
+
+    private static void harmadikfeladat()
+    {
+      Console.WriteLine("3. Feladat: Elemek száma", Elemek.Count());
+    }
+
+    private static void masodikfeladat()
+    {
+
+      Console.WriteLine("2. Feladat: adatok beolvasása");
+      StreamReader be = new StreamReader("felfedezesek.csv");
+
+
+      be.ReadLine();
+
+
+      while (!be.EndOfStream)
+      {
+
+        string[] a = be.ReadLine().Split(';');
+        Elemek.Add(new Elem(a[0], a[1], a[2],int.Parse(a[3]),a[4]));
+
+      }
+      be.Close();
+
+
+    }
+
+
+  }
+}
